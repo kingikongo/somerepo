8477c11b51b521ac0c3c254e72a64eb059a12f18
kingikongo <erdenkalilaeyev@gmail.com>
2025-05-18 05:03:53 +0300

    newfile

---
+++ somehwhile.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+int main() {
+	int save[] = {1, 2, 3, 4, 5, 6, 6, 7, 8};
+	int k = 4;
+	int i = 0;
+
+	while (save[i] == k) {
+		i += 1;	
+	
+	}
+	printf("Loop exited at i = %d\n", i);
+}
