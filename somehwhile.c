bfd770aabee6c85ad94fc33ceae5a123a04afedc
kingikongo <erdenkalilaeyev@gmail.com>
2025-05-18 05:06:09 +0300

    some

--- somehwhile.c
+++ somehwhile.c
@@ -1,9 +1,14 @@
-#include <studio.h>
+#include <stdio.h>
 
 int main() {
 	int save[] = {1, 2, 3, 4, 5, 6, 6, 7, 8};
 	int k = 4;
 	int i = 0;
+
+
+
+
+	printf("Hello world!");	
 
 	while (save[i] == k) {
 		i += 1;	
